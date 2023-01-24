41cd8d9a493148d7521b6a61f828d228f45d6337
maythetsoe <maythetsoe1994@gmail.com>
2023-01-24 20:34:30 +0900

    MiniRoom機能テストコード

--- spec/factories/minirooms.rb
+++ spec/factories/minirooms.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :miniroom do
-    
+    title {Faker::Lorem.sentence}
+    concept {Faker::Lorem.sentence}
+
+    after(:build) do |miniroom|
+      miniroom.image.attach(io: File.open('public/images/simple.jpeg'), filename: 'simple.jpeg')
+    end
+
+    association :user
   end
 end
--- spec/models/miniroom_spec.rb
+++ spec/models/miniroom_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Miniroom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @miniroom = FactoryBot.build(:miniroom)
+  end
+
+  describe "Miniroom機能" do
+    context 'Miniroom投稿できる' do
+      it 'title,concept,imageが存在すれば投稿できる' do
+        expect(@miniroom).to be_valid
+      end
+    end
+    context '投稿できないとき' do
+      it 'titleが空では投稿できない' do
+      @miniroom.title = ''
+      @miniroom.valid?
+      expect(@miniroom.errors.full_messages).to include "Title can't be blank"
+    end
+    it 'conceptが空では投稿できない' do
+    @miniroom.concept = ''
+    @miniroom.valid?
+    expect(@miniroom.errors.full_messages).to include "Concept can't be blank"
+  end
+    it 'imageが空では出品できない' do
+      @miniroom.image = nil
+      @miniroom.valid?
+      expect(@miniroom.errors.full_messages).to include "Image can't be blank"
+    end
+    it 'ユーザーが紐付いていないと投稿できない' do
+      @miniroom.user = nil
+      @miniroom.valid?
+      expect(@miniroom.errors.full_messages).to include('User must exist')
+      end
+  end
+end
 end
